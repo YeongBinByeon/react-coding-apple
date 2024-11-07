683c9b4b05559dd534bd13038661f3f2fe50233a
yeongbin <yeongbin6483@gmail.com>
2024-11-08 06:40:14 +0900

    탭 UI 만들기 / 컴포넌트 전환 애니메이션 주는 법 커밋

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import { Nav } from "react-bootstrap";
 
 function Detail(props) {
   useEffect(() => {
@@ -9,6 +10,8 @@ function Detail(props) {
 
   const [count, setCount] = useState(0);
   const [alert, setAlert] = useState(true);
+  const [탭, 탭변경] = useState(0);
+  const [fade2, setFade2] = useState("");
 
   useEffect(() => {
     let timer = setTimeout(() => {
@@ -19,6 +22,13 @@ function Detail(props) {
     };
   }, []);
 
+  useEffect(() => {
+    setFade2("end");
+    return () => {
+      setFade2("");
+    };
+  }, []);
+
   let { id } = useParams();
 
   const 찾은상품 = props.shoes.find((item) => {
@@ -26,7 +36,7 @@ function Detail(props) {
   });
 
   return (
-    <div className="container">
+    <div className={`container start ${fade2}`}>
       {alert === true ? (
         <div className="alert alert-warning">2초이내 구매시 할인</div>
       ) : null}
@@ -50,6 +60,60 @@ function Detail(props) {
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link0"
+            onClick={() => {
+              탭변경(0);
+            }}
+          >
+            버튼0
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link1"
+            onClick={() => {
+              탭변경(1);
+            }}
+          >
+            버튼1
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link2"
+            onClick={() => {
+              탭변경(2);
+            }}
+          >
+            버튼2
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <TabContent 탭={탭} />
+    </div>
+  );
+}
+
+function TabContent(props) {
+  let [fade, setFade] = useState("");
+
+  useEffect(() => {
+    let a = setTimeout(() => {
+      setFade("end");
+    }, 100);
+    return () => {
+      clearTimeout(a);
+      setFade("");
+    };
+  }, [props.탭]);
+
+  return (
+    <div className={`start ${fade}`}>
+      {[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][props.탭]}
     </div>
   );
 }
